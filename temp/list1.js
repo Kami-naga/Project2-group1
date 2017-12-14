28eebb03fc246955b79688e73bdc5e3a6a8660fd
Lin Yixuan <2442548803@sjtu.edu.cn>
2017-12-14 13:57:13 +0800

    Temporary push

---
+++ temp/list1.js
@@ -0,0 +1,36 @@
+import {Table} from 'react-bootstrap';
+import React from 'react';
+import { Component } from 'react';
+
+class List1 extends React.Component{
+    render(){
+        return(
+            <Table striped bordered condensed hover>
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>summary</th>
+                    <th>Link</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                </tbody>
+            </Table>
+        )
+    }
+}
+
+export default List1;
