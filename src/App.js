79afef0b3d8a95e9450553584d9708f6634942a1
Hokhy Tann <hokhytann017@gmail.com>
2017-12-25 23:14:43 +0800

    Checked

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Navibar from './navibar'
 
-
 class App extends React.Component {  //定义组件，也可以用React.createClass方法创建组件
     render() {
         return (
